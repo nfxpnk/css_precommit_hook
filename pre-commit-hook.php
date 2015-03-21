f6fd68228227d392b204d488433119803eaa9221
nfxpnk <nfxpnk@gmail.com>
2015-03-21 21:04:40 +0200

    Code refactoring (untested)

--- pre-commit-hook.php
+++ pre-commit-hook.php
@@ -1,187 +1,124 @@
 <?php
-//c:\Users\user\Dropbox\apps\server\php\php.exe -f h:\pre-commit.php
-$data = var_export($argv, true);
+$cssLintCliPath = 'csslint';
+$cssCombCliPath = 'csscomb';
+$cssCombConfigFilePath = 'h:/config-csscomb.json';
+$diffCliPath = 'C:/Users/user/AppData/Local/Programs/Git/bin/diff.exe';
+$tempDirectory = 'h:/temp';
+$patchFilePath = $tempDirectory . '/' . 'pre-commit.patch';
 
-$data .= "\n\n" . '=======================================111=============================================>' . "\n\n\n\n";
-$data .= file_get_contents($argv[1]);
-$data .= "\n\n" . '=======================================111 strlen=========================================>' . "\n\n\n\n";
-$data .= strlen(file_get_contents($argv[1]));
-$data .= "\n\n" . '========================================333 =========================================>' . "\n\n\n\n";
-$data .= file_get_contents($argv[3]);
-$data .= "\n\n" . '========================================333 strlen=========================================>' . "\n\n\n\n";
-$data .= strlen(file_get_contents($argv[3]));
-$data .= "\n\n" . '=====================================================================================>' . "\n\n\n\n";
-file_put_contents('h:/test.txt', $data);
-
-$files = file($argv[1]);
-
-ob_start();
-
-
-foreach($files as $key => $file) {
-	$file = trim($file);
-	$files[$key] = $file;
-
-	if(empty($file)) unset($files[$key]);
-	if(!file_exists($file)) unset($files[$key]);
+if(empty($argv)) {
+	echo 'Wrong execution.';
+	exit;
 }
 
-var_dump($files);
+$filesToCommitFilePath = trim($argv[1]);
+$filesToVerify = file($filesToCommitFilePath);
 
-file_put_contents('h:/test.txt', ob_get_contents(), FILE_APPEND);
-ob_end_clean();
+foreach($filesToVerify as $key => $filePath) {
+	$filePath = trim($filePath);
+	$filesToVerify[$key] = $filePath;
 
+	# Ignore not *.css files
+	if(!preg_macth("#\.css$#i", $filePath)) unset($filesToVerify[$key]);
 
-//$filesString = '"' . implode('" "', $files) . '"';
+	# Ignore empty lines
+	if(empty($filePath)) unset($filesToVerify[$key]);
 
-$errors = false;
-ob_start();
-foreach($files as $file) {
-	system('csslint --errors=known-properties,errors "' . $file . '"');
-	$cssLinkOutput = ob_get_contents();
-	$cssLinkOutput = trim($cssLinkOutput);
-	if(strpos($cssLinkOutput, 'csslint: No errors') !== 0) {
-		$errors = true;
-		break;
+	# Ignore deleted files
+	if(!file_exists($filePath)) unset($filesToVerify[$key]);
+}
+
+foreach($filesToVerify as $filePath) {
+	# File verification: csslint
+	ob_start();
+	system($cssLintCliPath . ' --errors=known-properties,errors "' . $filePath . '"');
+	$cssLintOutput = ob_get_contents();
+	ob_end_clean();
+
+	$cssLintOutput = trim($cssLintOutput);
+	if(strpos($cssLintOutput, 'csslint: No errors') !== 0) {
+		exitWithError($cssLintOutput);
 	}
-}
-ob_end_clean();
 
-file_put_contents('h:/test.txt', $cssLinkOutput, FILE_APPEND);
+	# File verification: csscomb
+	$fileBasename = basename($filePath);
+	$tempCssCombFilePath = $tempDirectory . '/' . preg_replace('#\.css$#i', '.csscomb.css', $fileBasename);
+	$tempOriginalFilePath = $tempDirectory . '/' . $fileBasename;
 
-function getLine($match, $code) {
-	return substr_count($code, "\n", 0, strpos($code, $match)) + 1;
-}
-/*
-if($errors == false) {
-	foreach($files as $file) {
-		$cssCode = file_get_contents($file);
-		$cssCode = preg_replace("#url\('data\:(.+?)'\)#msi", "url('base64')", $cssCode);
-		
-		// {      }
-		if(preg_match("#\{(\s*)\}#ms", $cssCode, $m)) {
-			$errors = true;
-			$line = getLine($m[0], $cssCode);
-			$cssLinkOutput = 'Empty {}! on line:' . $line;
-			break;
-		}
-		
-		// : ;!\n
-		if(preg_match("#\:[^;]+\n#msi", $cssCode, $m)) {
-			$errors = true;
-			$line = getLine($m[0], $cssCode);
-			$cssLinkOutput = 'no ;!! on line:' . $line;
-			break;
-		}
-		
-		
-		// find color hex if not lower
-		// ^....\t{2} error
-		if(preg_match("#^#msi", $cssCode, $m)) {
-			$errors = true;
-			$line = getLine($m[0], $cssCode);
-			$cssLinkOutput = 'no ;!! on line:' . $line;
-			break;
-		}
-		// после } должно быть два \n
-		// if strpos "  == fail
-		// find :[notspace] or more than one or tab fail
-		// 
+	# Copy original file to temporary folder (for csscomb and diffutils)
+	copy($filePath, $tempCssCombFilePath);
+	copy($filePath, $tempOriginalFilePath);
+
+	# Modify csscomb file for better results
+	$tempCssCombFileContent = file_get_contents($tempCssCombFilePath);
+
+	# Replace all comments into placeholders !$$!comments_var_!$$!
+	preg_match_all("#\/\*(.*?)\*\/#msi", $tempCssCombFileContent, $matches);
+	foreach($matches[0] as $key => $match)
+	{
+		$tempCssCombFileContent = str_replace($match, '!$$!comments_var_' . $key.'!$$!', $tempCssCombFileContent);
 	}
-}*/
 
-if($errors == false) {
-	foreach($files as $file) {
-		$filename = basename($file);
-		$tempFile = 'h:/temp/'.$filename;
-		copy($file, $tempFile);
-		
-		$tempFileContent = file_get_contents($tempFile);
-		
-		
-		preg_match_all("#\/\*(.*?)\*\/#msi", $tempFileContent, $matches);
-		foreach($matches[0] as $key => $match)
-		{
-			$tempFileContent = str_replace($match, '!!comments_var_' . $key.'!$$!', $tempFileContent);
-		}
-		
-		$tempFileContent = preg_replace("#^[ \t]+#msi", '', $tempFileContent);
-		$tempFileContent = preg_replace("#[ \t]+(\r\n|\n)#msi", "\n", $tempFileContent);
-		$tempFileContent = preg_replace("#\}[ \t]#msi", '}', $tempFileContent);
-		
-		$tempFileContent = preg_replace("#[ \t]+#msi", ' ', $tempFileContent);
-		$tempFileContent = preg_replace("#^\s*(\r\n|\n)#mi", '', $tempFileContent);
-		
+	# Delete all spaces and tabs from start of line
+	$tempCssCombFileContent = preg_replace("#^[ \t]+#msi", '', $tempCssCombFileContent);
 
-		$tempFileContent = preg_replace("#}(\n|\r\n)+#msi", '}', $tempFileContent);
-		$tempFileContent = str_replace("}", "}\n\n", $tempFileContent);
-		
-		
-		//$tempFileContent = str_replace('!$$!', '!$$!' . "\n", $tempFileContent);
-		
-		
+	# Delete all spaces and tabs from end of line
+	$tempCssCombFileContent = preg_replace("#[ \t]+(\r\n|\n)#msi", "\n", $tempCssCombFileContent);
 
-		foreach($matches[0] as $key => $match)
-		{
-			$tempFileContent = str_replace('!!comments_var_' . $key.'!$$!', $match, $tempFileContent);
-		}
-		
-		$tempFileContent = preg_replace("#\*\/\s+\/\*#msi", "*/\n/*", $tempFileContent);
-		
-		$tempFileContent = str_replace("\r\n", "\n", $tempFileContent);
-		
-		file_put_contents($tempFile, $tempFileContent);
-		
-	
-		
-		ob_start();
-		system('csscomb --config h:/config-csscomb.json --verbose "' . $tempFile . '"');
-		$csscomb = ob_get_contents();
-		$csscomb = trim($csscomb);
-		ob_end_clean();
-		
-		$originalFileContent = file_get_contents($file);
-		$originalFileContent = str_replace("\r\n", "\n", $originalFileContent);
-		
-		file_put_contents('h:/temp/originalFile.css', $originalFileContent);
-		
-		system('C:/Users/user/AppData/Local/Programs/Git/bin/diff.exe -Naur "'.'h:/temp/originalFile.css'.'" "'.$tempFile.'" > "h:/temp/commit.patch"');
-		
-		
-		$cssLinkOutput = file_get_contents('h:/temp/commit.patch');
-		$cssLinkOutput = trim($cssLinkOutput);
-		
-		if(!empty($cssLinkOutput)){
-			$cssLinkOutput = 'File not perfect. See: h:/temp/commit.patch';
-			$errors = true;
-			break;
-		}
-		
-		
-		/*
-		$tempFileLines = file($tempFile);
-		$sourceFileLines = file($file);
-		foreach($sourceFileLines as $key => $line) {
-			//$line = trim($line);
-			$cssCombLine = $tempFileLines[$key];
-			if($line != $cssCombLine) {
-				$errors = true;
-				$cssLinkOutput = ($key+1) . ' :: '. $line;
-				break;
-			}
-			
-		}
-		if($errors) break;*/
-		
+	# Delete all spaces and tabs after }
+	$tempCssCombFileContent = preg_replace("#\}[ \t]#msi", '}', $tempCssCombFileContent);
+
+	# Replace spaces or tabs > 2 into one space
+	$tempCssCombFileContent = preg_replace("#[ \t]+#msi", ' ', $tempCssCombFileContent);
+
+	# Remove empty lines
+	$tempCssCombFileContent = preg_replace("#^\s*(\r\n|\n)#mi", '', $tempCssCombFileContent);
+
+	# Remove newlines after }
+	$tempCssCombFileContent = preg_replace("#}(\n|\r\n)+#msi", '}', $tempCssCombFileContent);
+
+	# Add two newlines character after }
+	$tempCssCombFileContent = str_replace("}", "}\n\n", $tempCssCombFileContent);
+
+	# Put back our css comments instead of out placeholders
+	foreach($matches[0] as $key => $match)
+	{
+		$tempCssCombFileContent = str_replace('!$$!comments_var_' . $key.'!$$!', $match, $tempCssCombFileContent);
+	}
+
+	# Format comments newlines
+	$tempCssCombFileContent = preg_replace("#\*\/\s+\/\*#msi", "*/\n/*", $tempCssCombFileContent);
+
+	# Format all newlines to \n only
+	$tempCssCombFileContent = str_replace("\r\n", "\n", $tempCssCombFileContent);
+
+	# Write modified data to file
+	file_put_contents($tempCssCombFilePath, $tempCssCombFileContent);
+
+	# Execute csscomb
+	system($cssCombCliPath . ' --config "' . $cssCombConfigFilePath . '" "' . $tempCssCombFilePath . '"');
+
+	# Modify temp original file
+	$tempOriginalFileContent = file_get_contents($tempOriginalFilePath);
+	$tempOriginalFileContent = str_replace("\r\n", "\n", $tempOriginalFileContent);
+	file_put_contents($tempOriginalFilePath, $tempOriginalFileContent);
+
+	# Create *.patch file to see is there anything needs to be fixed
+	system($diffCliPath . ' -Naur "' . $tempOriginalFilePath . '" "' . $tempCssCombFilePath . '" > "' . $patchFilePath . '"');
+
+	$patchFileSize = filesize($patchFilePath);
+
+	if($patchFileSize > 0) {
+		# TODO: Modify patch file
+
+		exitWithError('File ' . $filePath . ' is not perfect.' . "\n" . 'See: ' . $patchFilePath . ' for details.');
 	}
 }
 
-
-
-
-if($errors) {
+function exitWithError($errorMessage) {
 	$stderr = fopen('php://stderr', 'w');
-	fwrite($stderr, $cssLinkOutput);
+	fwrite($stderr, $errorMessage);
+	fclose($stderr);
 	exit(1);
 }
 
