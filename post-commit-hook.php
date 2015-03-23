d66448ae05ae7776dabd25e91b4e0d05a10bc7fe
nfxpnk <nfxpnk@gmail.com>
2015-03-23 22:21:58 +0200

    Post-commit hook

--- post-commit-hook.php
+++ post-commit-hook.php
@@ -1,1 +1,19 @@
 <?php
+$revision = trim($argv[4]);
+$previousRevision = $revision - 1;
+
+$commitMessage = file_get_contents($argv[3]);
+$commitMessage = trim($commitMessage);
+
+preg_match("#^[A-Z]{3,5}\-\d{1,6}#", $commitMessage, $issueKey);
+if(!empty($issueKey[0])) {
+	$patchFile = 'h:/temp/patches/' . $revision . '_-_' . $issueKey[0] . '.patch';
+	system('svn diff -r ' . $previousRevision . ':' . $revision . ' > "' . $patchFile . '"');
+}
+
+function exitWithError($errorMessage) {
+	$stderr = fopen('php://stderr', 'w');
+	fwrite($stderr, $errorMessage);
+	fclose($stderr);
+	exit(1);
+}
