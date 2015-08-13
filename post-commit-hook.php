c43d86bef33bee48e5c8fa5b78f2d0156bbf5cdb
nfxpnk <nfxpnk@gmail.com>
2015-08-13 15:56:41 +0300

    Ignore whitespace and eol in patch file

--- post-commit-hook.php
+++ post-commit-hook.php
@@ -8,7 +8,7 @@ $commitMessage = trim($commitMessage);
 preg_match("#^[A-Z]{3,7}\-\d{1,7}#", $commitMessage, $issueKey);
 if(!empty($issueKey[0])) {
 	$patchFile = 'h:/temp/patches/' . $revision . '_-_' . $issueKey[0] . '.patch';
-	system('svn diff -r ' . $previousRevision . ':' . $revision . ' > "' . $patchFile . '"');
+	system('svn diff -x "-w --ignore-eol-style" -r ' . $previousRevision . ':' . $revision . ' > "' . $patchFile . '"');
 }
 
 function exitWithError($errorMessage) {
